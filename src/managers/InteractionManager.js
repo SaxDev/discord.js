a3f08db68400948399d83e18b85b8933d5212991
SaxDev <30241194+SaxDev@users.noreply.github.com>
2021-01-05 19:12:20 +0300

    Create InteractionManager.js

---
+++ src/managers/InteractionManager.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const BaseManager = require('./BaseManager');
+const { TypeError } = require('../errors');
+const Interaction = require('../structures/Interaction');
+const Collection = require('../util/Collection');
+
+/**
+ * Manages API methods for Interactions and holds their cache.
+ * @extends {BaseManager}
+ */
+class InteractionManager extends BaseManager {
+  constructor(channel, iterable) {
+    super(channel.client, iterable, Interaction, Collection);
+    /**
+     * The channel that the interaction belong to
+     * @type {TextBasedChannel}
+     */
+    this.channel = channel;
+  }
+  
+  /**
+   * The cache of Interactions
+   * @type {Collection<Snowflake, Interaction>}
+   * @name InteractionManager#cache
+   */
+
+  add(data, cache) {
+    return super.add(data, cache, { extras: [this.channel] });
+  }
+}
+
+module.exports = InteractionManager;
