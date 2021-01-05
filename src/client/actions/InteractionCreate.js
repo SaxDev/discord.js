8630034a02f7367781d3bc7069677bcf5c9d01f5
SaxDev <30241194+SaxDev@users.noreply.github.com>
2021-01-05 19:44:02 +0300

    Update InteractionCreate.js

--- src/client/actions/InteractionCreate.js
+++ src/client/actions/InteractionCreate.js
@@ -5,14 +5,23 @@ const { Events } = require('../../util/Constants');
 
 class InteractionCreateAction extends Action {
   handle(data) {
-    const channel = this.client.channels.get(data.channel_id);
+    const client = this.client;
+    const channel = client.channels.cache.get(data.channel_id);
+    if (channel) {
+      const existing = channel.interactions.cache.get(data.id);
+      if (existing) return { interaction: existing };
+      const interaction = channel.interactions.add(data);
+      const user = interaction.author;
+      let member = interaction.member;
+    }
+    
     /**
      * Emitted whenever a interaction is created.
      * @event Client#interactionCreate
      * @param {Interaction} The interaction that was created      
      */
-    this.client.emit(Events.INTERACTION_CREATE, data, channel);
-    return { data };
+    this.client.emit(Events.INTERACTION_CREATE, interaction);
+    return { interaction };
   }
 }
 
