60169a5d5260fdfef034d10b1bd1f69cbcd45905
SaxDev <30241194+SaxDev@users.noreply.github.com>
2021-01-05 23:18:50 +0300

    Update Constants.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -231,11 +231,11 @@ exports.ApplicationCommandOptionTypes = {
 };
 
 exports.InteractionResponseType = {
-  'Pong': 1,
-  'Acknowledge': 2,
-  'ChannelMessage': 3,
-  'ChannelMessageWithSource': 4,
-  'AcknowledgeWithSource': 5
+  Pong: 1,
+  Acknowledge: 2,
+  ChannelMessage: 3,
+  ChannelMessageWithSource: 4,
+  AcknowledgeWithSource: 5
 };
   
 exports.Events = {
